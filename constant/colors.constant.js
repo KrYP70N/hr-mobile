1fe30e5e6914861b91c9ef6d45b64b9f8e92ea81
kyawmyohtut <kyawmyohtut@innovix-solutions.com>
2020-03-03 14:39:44 +0630

    separate version

--- constant/colors.constant.js
+++
@@ -1,9 +0,0 @@
-export default {
-    primary: '#377375',
-    secondary: '#333',
-    placeholder: '#aaa',
-    light: '#fff',
-    light2: '#ccc',
-    negative: '#FF0101',
-    warning: '#FFB300'
-}
