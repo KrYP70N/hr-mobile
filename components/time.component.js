f9538112ec2a7f221392989505e4445894fbdef5
phyu thin kyi <phyuthinkyi@innovix-solutions.com>
2020-05-22 12:18:14 +0630

    Time Componet fix

--- components/time.component.js
+++ components/time.component.js
@@ -29,6 +29,7 @@ export default class Clock extends Component {
                         // request time
                         APIs.Time(endPoint, key, id)
                             .then((response) => {
+                                console.log("Time Response Data", response.data)
                                 if (response.status === 'success') {
                                     this.setState({
                                         time: Moment(response.data)
