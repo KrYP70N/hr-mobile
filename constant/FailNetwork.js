1d3ea54107009882f80781dfa9dcbed58c489547
phyu thin kyi <phyuthinkyi@innovix-solutions.com>
2020-08-17 16:52:36 +0630

    add Network Background Image

--- constant/FailNetwork.js
+++ constant/FailNetwork.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, ImageBackground } from 'react-native';
 
 class FailNetwork extends Component {
   constructor(props) {
@@ -11,8 +11,7 @@ class FailNetwork extends Component {
   render() {
     return (
       <View style = {{flex: 1, justifyContent: 'center', alignItems: 'center', padding: 20}}>
-       <Image source = {require('../assets/icon/off.png')} />
-       <Text style = {{marginTop: 30, fontSize: 20, textAlign: 'center'}}>Mobile network is not available!. Please open your network!.</Text>
+       <ImageBackground style = {{width: '100%', height: '100%'}} source={require('../assets/icon/no_internet.png')} />
       </View>
     );
   }
